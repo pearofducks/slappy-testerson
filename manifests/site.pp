139ebd3c86f8d9460ff9ceffae21341ade7394e0
pearofducks <pearofducks@gmail.com>
2013-08-29 09:04:03 +0200

    Basic tests and packages added

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,27 +53,13 @@ Homebrew::Formula <| |> -> Package <| |>
 
 node default {
   # core modules, needed for most things
-  include dnsmasq
   include git
-  include hub
-  include nginx
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
   }
 
-  # node versions
-  include nodejs::v0_4
-  include nodejs::v0_6
-  include nodejs::v0_8
-  include nodejs::v0_10
-
-  # default ruby versions
-  include ruby::1_8_7
-  include ruby::1_9_2
-  include ruby::1_9_3
-  include ruby::2_0_0
 
   # common, useful packages
   package {
@@ -88,4 +74,8 @@ node default {
     ensure => link,
     target => $boxen::config::repodir
   }
+
+  include skype
+  include opera
+  include googledrive
 }
